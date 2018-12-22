9deaee4cd4341c4d79bbce47626c6a95b67143a7
omrinao <38496755+omrinao@users.noreply.github.com>
2018-12-22 20:26:08 +0200

    Add files via upload

---
+++ src/Main/java/Searching/ARanker.java
@@ -0,0 +1,205 @@
+package Searching;
+
+import Indexing.DocumentDictionary;
+import Indexing.PostingTermData;
+import Indexing.TermData;
+import Parse.IntWrapper;
+
+import java.io.*;
+import java.util.*;
+
+public abstract class ARanker implements IRanker  {
+
+    protected HashMap<String, TermData> _corpusDictionary;
+    protected DocumentDictionary _documentDictionary;
+    protected List<String> _cities;
+    protected int _totalDocNum;
+    protected double _avgDocLength;
+    protected boolean _stemming;
+    protected String _outputPath;
+
+    protected final String _termsPosting = "FinalPosting";
+    protected final String _docsPosting = "DocumentPosting";
+    protected final String _stem = "STEM";
+    protected final String _txt = ".txt";
+
+
+    protected ARanker(HashMap<String, TermData> _corpusDictionary, List<String> _cities, DocumentDictionary docDict, boolean _stemming, String _outputPath) {
+        this._corpusDictionary = _corpusDictionary;
+        this._cities = _cities;
+        this._documentDictionary = docDict;
+        this._totalDocNum = docDict.get_totalDocCount();
+        this._avgDocLength = docDict.get_avgDocLength();
+        this._stemming = _stemming;
+        this._outputPath = _outputPath;
+    }
+
+    /**
+     * method to retrieve all data of the query's terms
+     * @param query - the map of the query
+     * @return - map of term and its posting data
+     */
+    protected HashMap<String, List<PostingTermData>> extractTermsData(HashMap<String, IntWrapper> query){
+        String path = _outputPath + _termsPosting;
+        if (_stemming){
+            path = path + _stem;
+        }
+        path = path + _txt;
+
+        HashMap<String, List<PostingTermData>> toReturn = new HashMap<>();
+
+        try (
+                RandomAccessFile ra = new RandomAccessFile(path, "r");
+        ) {
+            for (String term :
+                    query.keySet()){
+                if (!_corpusDictionary.containsKey(term)){
+                    continue;
+                }
+
+                long pointer = _corpusDictionary.get(term).getM_pointer();
+                ra.seek(pointer);
+                String allPostingData = ra.readLine();
+                int start = allPostingData.indexOf("#~");
+                if (start <=0 ){
+                    System.out.println("bad pointer!!");
+                    continue;
+                }
+
+                List<PostingTermData> totalTermOccurrences = new ArrayList<>();
+                allPostingData = allPostingData.substring(start+2, allPostingData.length()-1);
+                String[] splitByDoc = allPostingData.split(",");
+
+                for (String aSplitByDoc : splitByDoc) {
+                    String[] splitByAttributes = aSplitByDoc.split(" ");
+                    PostingTermData termData = new PostingTermData(splitByAttributes[0], Integer.valueOf(splitByAttributes[1]),
+                            Byte.valueOf(splitByAttributes[2]), Byte.valueOf(splitByAttributes[3]));
+
+                    totalTermOccurrences.add(termData);
+                }
+
+                toReturn.put(term, totalTermOccurrences);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * with a given map of terms and where they occurred, returns a map of docs and terms with their data
+     * @param termsData - the mapping of terms and where they occurred
+     * @return - mapping of docs and terms with their data
+     */
+    protected HashMap<String, HashMap<String, PostingTermData>> getDocumentsTerms(HashMap<String, List<PostingTermData>> termsData){
+        HashMap<String, HashMap<String, PostingTermData>> toReturn = new HashMap<>();
+
+        for (String term :
+                termsData.keySet()){
+            List<PostingTermData> termsList = termsData.get(term);
+
+            for (PostingTermData data :
+                    termsList){
+                HashMap<String, PostingTermData> documentTerms = toReturn.computeIfAbsent(data.get_doc(), k -> new HashMap<>()); // getting the terms of the doc if exist
+                documentTerms.put(term, data);
+            }
+        }
+        return toReturn;
+    }
+
+    /**
+     * given an ordered set of docNums will return their posting data
+     * @param docNums - ordered set of doc numbers
+     * @return - set of the corresponding posting data
+     */
+    protected Set<RetrievedDocument> docsMatchingCity(TreeSet<Integer> docNums){
+        Set<RetrievedDocument> matchingDoc = new LinkedHashSet<>();
+        String path = _outputPath + _docsPosting;
+        if (_stemming){
+            path = path +_stem;
+        }
+        path = path+_txt;
+
+        try (
+                RandomAccessFile ra = new RandomAccessFile(path, "r")
+                ){
+            long pointer = 0;
+            for (Integer doc :
+                    docNums){
+                pointer = _documentDictionary.getPointer(doc);
+                String officialName = _documentDictionary.getName(doc);
+                if (pointer == -1){ // should never enter here
+                    System.out.println("document have not been processed properly" + doc);
+                    throw new IOException("bad pointer");
+                }
+
+                ra.seek(pointer);
+                String data = ra.readLine();
+
+                String[] docData = data.split(" ");
+                short startLine = Short.valueOf(docData[0]);
+                short endLine = Short.valueOf(docData[1]);
+                String relative = docData[2];
+                int unique = Integer.valueOf(docData[3]);
+                int maxTF = Integer.valueOf(docData[4]);
+                int length = Integer.valueOf(docData[5]);
+                String city = docData[6];
+
+                if (_cities==null || _cities.contains(city)){
+                    RetrievedDocument retrievedDocument = new RetrievedDocument();
+                    retrievedDocument.set_city(city);
+                    retrievedDocument.set_docNum(String.valueOf(doc));
+                    retrievedDocument.set_length(length);
+                    retrievedDocument.set_startLine(startLine);
+                    retrievedDocument.set_endLine(endLine);
+                    retrievedDocument.set_file(relative);
+                    retrievedDocument.set_docName(officialName);
+
+                    matchingDoc.add(retrievedDocument);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        return matchingDoc;
+    }
+
+    /**
+     * method that adds rank BM25 rank to set of retrieved docs
+     * @param allDocs - the entire docs with matching query terms
+     * @param cityDocs - docs that match
+     */
+    protected void rankByBM25(HashMap<String, HashMap<String, PostingTermData>> allDocs, Set<RetrievedDocument> cityDocs, HashMap<String, IntWrapper> query, double value){
+
+        double k = 2;
+        double b = 0.75;
+
+        for (RetrievedDocument document:
+                cityDocs){
+            HashMap<String, PostingTermData> docMatchingTerms = allDocs.get(document.get_docNum());
+
+            if (docMatchingTerms==null) // should never enter here
+                continue;
+
+            double rank = 0;
+            for (String termInDoc:
+                    docMatchingTerms.keySet()){
+                int termCount = docMatchingTerms.get(termInDoc).get_termOccurrences();
+                double normalizedDocLength = (document._length/_avgDocLength);
+
+                int wordQueryCount = query.get(termInDoc).get_value();
+                double numerator = (k+1)*termCount;
+                double denominator = termCount + k*(1-b+b*normalizedDocLength);
+                double normalizedDF = Math.log10((_totalDocNum+1)/_corpusDictionary.get(termInDoc).getM_df());
+
+                rank += (wordQueryCount*(numerator/denominator)*normalizedDF);
+            }
+            document.add_rank(value*rank);
+        }
+    }
+
+}
---
+++ src/Main/java/Searching/RankerNoSemantics.java
@@ -0,0 +1,45 @@
+package Searching;
+
+import Indexing.DocumentDictionary;
+import Indexing.PostingTermData;
+import Indexing.TermData;
+import Parse.IntWrapper;
+
+import java.io.RandomAccessFile;
+import java.util.*;
+
+public class RankerNoSemantics extends ARanker {
+
+    public RankerNoSemantics(HashMap<String, TermData> _corpusDictionary, List<String> _cities, DocumentDictionary docDict, boolean _stemming, String _outputPath) {
+        super(_corpusDictionary, _cities, docDict, _stemming, _outputPath);
+    }
+
+
+    @Override
+    public PriorityQueue<RetrievedDocument> rank(HashMap<String, IntWrapper> query, List<String> cities){
+        PriorityQueue<RetrievedDocument> toReturn = new PriorityQueue<>();
+
+        HashMap<String, List<PostingTermData>> termsInQuery = this.extractTermsData(query);
+        HashMap<String, HashMap<String, PostingTermData>> docsWithQueryTerms = this.getDocumentsTerms(termsInQuery);
+
+        TreeSet<Integer> orderedDocNum = new TreeSet<>();
+        for (String strNum :
+                docsWithQueryTerms.keySet()){
+            orderedDocNum.add(Integer.valueOf(strNum));
+        }
+
+        Set<RetrievedDocument> docsMatchingCity = this.docsMatchingCity(orderedDocNum);
+        this.rankByBM25(docsWithQueryTerms, docsMatchingCity, query, 1);
+
+        toReturn.addAll(docsMatchingCity);
+        if (toReturn.size() > 50){
+            PriorityQueue<RetrievedDocument> tmp = toReturn;
+            toReturn = new PriorityQueue<>();
+            while ( toReturn.size() <= 50){
+                toReturn.add(tmp.poll());
+            }
+        }
+
+        return toReturn;
+    }
+}
